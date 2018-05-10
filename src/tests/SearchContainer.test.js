d0e365140dd8edce326c7aace75280cec24a10fb
Huascar <huascarmc@hotmail.com>
2018-05-10 13:11:16 +0100

    added test should render without crashing search container

--- src/tests/SearchContainer.test.js
+++ src/tests/SearchContainer.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import MovieListContainer from '../containers/MovieListContainer'
+import SearchContainer from '../containers/SearchContainer'
 import { shallow, mount } from 'enzyme';
 import renderer from 'react-test-renderer';
 
 it('should render without crashing', () => {
-  shallow(<MovieListContainer />);
+  shallow(<SearchContainer />);
 });
