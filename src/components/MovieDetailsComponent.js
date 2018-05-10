07eaa718c48edf8d62e99c00c28c0a7d8d1a9797
Huascar <huascarmc@hotmail.com>
2018-05-10 18:24:46 +0100

    added more details for selected movie

--- src/components/MovieDetailsComponent.js
+++ src/components/MovieDetailsComponent.js
@@ -6,17 +6,26 @@ class MovieDetailsComponent extends React.Component {
     let actors;
     let awards;
     let poster;
+    let year;
+    let runtime;
+    let rating;
     if(this.props.movie !== undefined) {
       actors = this.props.movie.Actors;
       awards = this.props.movie.Awards;
       poster = this.props.movie.Poster;
+      year = this.props.movie.Year;
+      runtime = this.props.movie.Runtime;
+      rating = this.props.movie.imdbRating;
     }
     return(
       <div className="movie-details-container">
         <div>
           <img src={poster}/>
-          <p>{ awards }</p>
-          <p>{ actors }</p>
+          <p>Awards: { awards }</p>
+          <p>Actors: { actors }</p>
+          <p>Year: { year }</p>
+          <p>Runtime: { runtime }</p>
+          <p>Rating: { rating }</p>
         </div>
       </div>
     );
