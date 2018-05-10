7e93957fa446799a7f6343ad3b38e4d766b7e25a
Huascar <huascarmc@hotmail.com>
2018-05-10 13:16:05 +0100

    search container renders as app children sucess

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import MovieListContainer from './containers/MovieListContainer.js'
+import SearchContainer from './containers/SearchContainer.js'
 
 class App extends Component {
   render() {
     return (
+      <div>
         <MovieListContainer />
+        <SearchContainer />
+      </div>
     );
   }
 }
--- src/containers/SearchContainer.js
+++ src/containers/SearchContainer.js
@@ -6,6 +6,7 @@ class MovieListContainer extends React.Component {
   render() {
     return(
       <div className="search-wrapper">
+        a
       </div>
     );
   }
