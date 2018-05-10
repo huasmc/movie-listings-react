ebc524629c83ae0c54d070a2a713d0e10f61b971
Huascar <huascarmc@hotmail.com>
2018-05-10 18:17:50 +0100

    added poster to movie details container

--- src/components/MovieDetailsComponent.js
+++ src/components/MovieDetailsComponent.js
@@ -5,13 +5,16 @@ class MovieDetailsComponent extends React.Component {
   render() {
     let actors;
     let awards;
+    let poster;
     if(this.props.movie !== undefined) {
       actors = this.props.movie.Actors;
       awards = this.props.movie.Awards;
+      poster = this.props.movie.Poster;
     }
     return(
       <div className="movie-details-container">
         <div>
+          <img src={poster}/>
           <p>{ awards }</p>
           <p>{ actors }</p>
         </div>
