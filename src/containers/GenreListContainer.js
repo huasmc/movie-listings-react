e7945b5ab497c2b626e4f3a419d394843b774faf
Huascar <huascarmc@hotmail.com>
2018-05-10 16:52:11 +0100

    css updates

--- src/containers/GenreListContainer.js
+++ src/containers/GenreListContainer.js
@@ -5,10 +5,14 @@ class GenreListContainer extends React.Component {
 
   render() {
     return(
+      <div>
+        <p className="to-be-removed">double-click genre</p>
+
       <div className="genre-container">
         <GenreComponent genre={"comedy"} updateSearch={ this.props.updateSearch } />
         <GenreComponent genre={"action"} updateSearch={ this.props.updateSearch }/>
         <GenreComponent genre={"thriller"} updateSearch={ this.props.updateSearch }/>
+      </div>
       </div>
     );
   }
