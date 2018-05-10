c0b9785eccc22f42b35decb0b2388976f87811fa
Huascar <huascarmc@hotmail.com>
2018-05-10 10:52:02 +0100

    app should render movielistcontainer test fail

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow, mount } from 'enzyme';
+import renderer from 'react-test-renderer';
+import MovieListContainer from './containers/MovieListContainer'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+it('should render MovieListContainer', () => {
+  const wrapper = shallow(<App />);
+  const container = wrapper.find(MovieListContainer)
+  expect(container.length).toEqual(1);
 });
