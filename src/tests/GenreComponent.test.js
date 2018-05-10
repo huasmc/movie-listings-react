74cf21e72d7eeddc9b76af165fce12510e15aad7
Huascar <huascarmc@hotmail.com>
2018-05-10 13:29:21 +0100

    should render div with class genre-component test fail

--- src/tests/GenreComponent.test.js
+++ src/tests/GenreComponent.test.js
@@ -7,6 +7,14 @@ it('should render without crashing', () => {
   shallow(<GenreComponent />);
 });
 
+it('should render div with class genre-component', () => {
+  const wrapper = shallow(<GenreComponent />);
+  const div = wrapper.find('.genre-component')
+  expect(div.length).toEqual(1);
+});
+
+
+
 // it('should match the snapshot', () => {
 //   const component = renderer.create(<GenreComponent />);
 //   let tree = component.toJSON();
