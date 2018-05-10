9d1e1e668ba4460d51fe4370faaa7656615cd92b
Huascar <huascarmc@hotmail.com>
2018-05-10 10:53:39 +0100

    should render MovielistContainer test passed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import MovieListContainer from './containers/MovieListContainer'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <MovieListContainer />
       </div>
     );
   }
--- src/App.test.js
+++ src/App.test.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { shallow, mount } from 'enzyme';
 import renderer from 'react-test-renderer';
-import MovieListContainer from './containers/MovieListContainer'
+
 
 it('should render MovieListContainer', () => {
-  const wrapper = shallow(<App />);
-  const container = wrapper.find(MovieListContainer)
+  const wrapper = mount(<App />);
+  const container = wrapper.find('MovieListContainer')
   expect(container.length).toEqual(1);
 });
