184fe2a15868ffc1bc3eb06f1f88c82e1966f3db
Huascar <huascarmc@hotmail.com>
2018-05-10 18:49:51 +0100

    should render two divs test passed

--- src/tests/MovieDetailsComponent.test.js
+++ src/tests/MovieDetailsComponent.test.js
@@ -7,8 +7,16 @@ it('should render without crashing', () => {
   shallow(<MovieDetailsComponent />);
 });
 
+it('should render two divs', () => {
+  const wrapper = shallow(<MovieDetailsComponent />);
+  const div = wrapper.find('div')
+  expect(div.length).toEqual(2);
+})
+
 it('should have a div with movie-details-container selector', () => {
   const wrapper = shallow(<MovieDetailsComponent />);
   const div = wrapper.find('.movie-details-container')
   expect(div.length).toEqual(1);
 });
+
+// it('should have an image tag')
