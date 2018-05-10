8a38319de956bfc67a519e9f9ec3a8f19d277889
Huascar <huascarmc@hotmail.com>
2018-05-10 13:39:49 +0100

    integrated genre component with genrelist container

--- src/containers/GenreListContainer.js
+++ src/containers/GenreListContainer.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import GenreComponent from '../components/GenreComponent'
 
 class GenreListContainer extends React.Component {
 
   render() {
     return(
       <div className="genre-container">
-
+        <GenreComponent genre={"comedy"} />
+        <GenreComponent genre={"action"} />
+        <GenreComponent genre={"indie"} />
       </div>
     );
   }
