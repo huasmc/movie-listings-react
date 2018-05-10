c74f685cfd2ad969c3908558b2fa53b064199e02
Huascar <huascarmc@hotmail.com>
2018-05-10 13:13:55 +0100

    should render without crashing test passed

---
+++ src/containers/SearchContainer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import axios from 'axios'
+
+class MovieListContainer extends React.Component {
+
+  render() {
+    return(
+      <div className="search-wrapper">
+      </div>
+    );
+  }
+}
+
+export default MovieListContainer;
