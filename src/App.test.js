3946f23ae5f27ea385c39156ecd8d12914d8139a
Huascar <huascarmc@hotmail.com>
2018-05-10 13:59:53 +0100

    should render MovielistContainer test passed

--- src/App.test.js
+++ src/App.test.js
@@ -4,7 +4,7 @@ import App from './App';
 import { shallow, mount } from 'enzyme';
 import renderer from 'react-test-renderer';
 
-xit('should render MovieListContainer', () => {
+it('should render MovieListContainer', () => {
   const wrapper = mount(<App />);
   const container = wrapper.find('MovieListContainer')
   expect(container.length).toEqual(1);
