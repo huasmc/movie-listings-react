4edd5aade6e8eb4deda62621f5d883d05b9b663f
Huascar <huascarmc@hotmail.com>
2018-05-10 10:24:32 +0100

    created movielistcontainer, render test fail

---
+++ src/containers/MovieListContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class MovieListContainer extends React.Component {
+
+  render() {
+    return(
+      <div>
+    </div>
+    );
+  }
+}
+
+export default MovieListContainer;
