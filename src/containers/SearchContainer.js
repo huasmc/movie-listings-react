f16676beb41592eb3e85394b5cd28c0f25ed42eb
Huascar <huascarmc@hotmail.com>
2018-05-10 16:32:55 +0100

    integrated input with search contrainer success

--- src/containers/SearchContainer.js
+++ src/containers/SearchContainer.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import GenreListContainer from './GenreListContainer'
+import InputContainer from './InputContainer'
 
 class SearchContainer extends React.Component {
 
   render() {
     return(
       <div className="search-wrapper">
-        <GenreListContainer updateSearch={ this.props.updateSearch }/>
+        <InputContainer updateSearch={this.props.updateSearch}/>
+        <GenreListContainer updateSearch={ this.props.updateSearch } />
       </div>
     );
   }
