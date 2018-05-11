d4e84969dad1a7d700417fcd161fbfc1a9f565f6
Huascar <huascarmc@hotmail.com>
2018-05-11 16:42:41 +0100

    should render form with onSubmit prop test fail/skipped"

--- src/tests/InputContainer.test.js
+++ src/tests/InputContainer.test.js
@@ -25,6 +25,12 @@ it('should render form', () => {
  expect(form.exists()).toEqual(true);
 });
 
+xit('should render form with onSubmit prop', () => {
+ const wrapper = shallow(<InputContainer />);
+ const form = wrapper.find('form');
+ expect(JSON.stringify(form.prop('onSubmit'))).toEqual("");
+});
+
 it('should match the snapshot', () => {
  const component = renderer.create(
   <InputContainer />
