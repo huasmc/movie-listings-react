36100881305edfe00f821ee355388f34bd62b12d
Huascar <huascarmc@hotmail.com>
2018-05-12 19:49:08 +0100

    should render div with search-details-container test fail"

--- src/tests/SelectedImageContainer.test.js
+++ src/tests/SelectedImageContainer.test.js
@@ -7,6 +7,12 @@ it('should render without crashing', () => {
   shallow(<SelectedImageContainer />);
 });
 
+it('should render div with search-details-container class', () => {
+  const wrapper = shallow(<SelectedImageContainer />);
+  const div = wrapper.find('.search-details-container')
+  expect(div.length).toEqual(1);
+});
+
 xit('should match the snapshot', () => {
   const component = renderer.create(<SelectedImageContaine />);
   let tree = component.toJSON();
